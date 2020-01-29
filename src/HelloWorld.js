806414e556a51c496985b124bcdc37d9562f94d0
jorgeespino  Student <selectjorge@gmail.com>
2020-01-29 10:45:41 -0600

    finished drill 1

---
+++ src/HelloWorld.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    // Setting the initial state
+    state = {who:'world'}
+    render() { 
+    return <div>
+        <button onClick={()=> this.setWho('world')}>world </button> 
+        <button onClick={()=> this.setWho('friend')}>friend</button>
+        <button onClick={()=> this.setWho('react')}>react </button>
+        <p> Hello, {this.state.who} </p> </div>
+
+
+}
+setWho = who => {
+    // State is updated using setState
+    this.setState({
+        who
+    });
+};
+}
+export default HelloWorld;
